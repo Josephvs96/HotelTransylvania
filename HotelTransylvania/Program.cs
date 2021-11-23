a36b3ff3e1c9ac1206e0eba6811f0001b05631c3
Josephvs96 <bahhadi.joseph@gmail.com>
2021-11-23 11:33:35 +0100

    Add project files.

---
+++ HotelTransylvania/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelTransylvania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
