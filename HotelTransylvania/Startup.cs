e94011d6c646c0632c75a92bbfc8c123073d9381
Josephvs96 <bahhadi.joseph@gmail.com>
2021-11-23 11:49:16 +0100

    Configured dependicy injection

---
+++ HotelTransylvania/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting; 
+namespace HotelTransylvania
+{
+
+    internal class Startup
+    {
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) => { })
+                .ConfigureAppConfiguration((context, appConfiguration) => appConfiguration.Build());
+                    
+        }
+    }
+}
